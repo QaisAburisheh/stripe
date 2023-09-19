09fce1e82950e86507f6988250998a16276d0560
Qais Abburisheh <qais.aburisheh@gmail.com>
2023-09-19 11:41:05 +0300

    update railway url

--- routes/stripe.js
+++ routes/stripe.js
@@ -10,19 +10,17 @@ const stripe = Stripe(process.env.STRIPE_SECRET);
 const router = express.Router();
 
 const checkoutSuccessPage = fs.readFileSync(
-    path.join(__dirname, 'checkout-success.html')
-  );
-  router.get("/checkout-success", (req, res) => {
-    res.set("Content-Type", "text/html");
-    res.send(checkoutSuccessPage);
-  });
-  const checkoutCancel = fs.readFileSync(
-    path.join(__dirname, 'cancel.html')
-  );
-  router.get("/cancel", (req, res) => {
-    res.set("Content-Type", "text/html");
-    res.send(checkoutCancel);
-  });
+  path.join(__dirname, "checkout-success.html")
+);
+router.get("/checkout-success", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(checkoutSuccessPage);
+});
+const checkoutCancel = fs.readFileSync(path.join(__dirname, "cancel.html"));
+router.get("/cancel", (req, res) => {
+  res.set("Content-Type", "text/html");
+  res.send(checkoutCancel);
+});
 router.post("/create-checkout-session", async (req, res) => {
   const customer = await stripe.customers.create({
     metadata: {
@@ -49,16 +47,16 @@ router.post("/create-checkout-session", async (req, res) => {
   });
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-   
-    
+
     phone_number_collection: {
       enabled: false,
     },
     line_items,
     mode: "payment",
     customer: customer.id,
-    success_url: "https://paymentorders-production.up.railway.app/stripe/checkout-success",
-    cancel_url:  "https://paymentorders-production.up.railway.app/stripe/cancel",
+    success_url:
+      "https://stripe-production-850c.up.railway.app//stripe/checkout-success",
+    cancel_url: "https://paymentorders-production.up.railway.app/stripe/cancel",
   });
 
   // res.redirect(303, session.url);
@@ -156,7 +154,5 @@ const createOrder = async (customer, data) => {
 //     res.status(200).end();
 //   }
 // );
-
-
 
 module.exports = router;
